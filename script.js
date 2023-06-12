010fe029ac439e5487a68a0f2f09cadd6d49e30b
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-12 15:17:29 -0500

    My Transformer WebSite

---
+++ script.js
@@ -0,0 +1,23 @@
+let myJam = {
+   gospel: {
+        name: "gospel",
+        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROmozVxPesceOVpgVhC19-YEjAJl5sLJhvd8luftynBzGFJJe9xVkLt04YgSFG3HZz_TM&usqp=CAU",
+        description: " Good Music Soothes the soul. Enjoy a beautiful blend of different cultural music across Cameroon and some African countries",
+        video: "https://i.ytimg.com/vi/og77TBWT57M/hqdefault.jpg",
+       type: "Praise and Worship"
+    },
+   coutry: {
+        name: "country",
+        Image: "https://media1.giphy.com/media/sWog4ArVlts8Lkj361/giphy.gif",
+        description: " Good Music Soothes the soul. ",
+        video: "https://i.ytimg.com/vi/ZEWGyyLiqY4/maxresdefault.jpg",
+       type: "Soul and Heartfelt"
+    },
+    classical: {
+        name: "classical",
+        Image: "https://media0.giphy.com/media/3ohhwBL1q1I66srvAA/giphy.gif",
+        description: " Good Music Soothes the soul.",
+        video: "https://youtu.be/IUZEtVbJT5c",
+       type: "Enchanting and blissful"
+    }
+}
